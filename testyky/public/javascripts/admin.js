66fb48079ed99e0c682983b4bc889bb8b7ec7ade
alex-sch-acc <schneiderua@gmail.com>
2015-12-24 19:16:36 +0200

    updated

--- testyky/public/javascripts/admin.js
+++ testyky/public/javascripts/admin.js
@@ -13,6 +13,64 @@ function testUrl () {
 
 /*********************************/
 
+// Checking  examinationType + hide/unhide newUserData
+$(".examinationType select").on( "click", function() {
+		var someVal = $(".examinationType select").val();
+
+		if (someVal == "Індивідуальне") {
+			$('.newUserData').show("fast");
+		}
+		else {
+			$('.newUserData').hide("fast");	
+		}
+});
+
+
+// update pauseDuration
+$('.testSelect input').on('change', function(event) {
+	event.preventDefault();
+		var toChange = $(this).val();
+		$(this).prev().text(toChange);
+		console.log($(this).prev().text());
+});
+
+
+// get db names + form test list
+
+
+
+
+
+
+
+$('.submitNewTest').on('click', function(event) {
+	event.preventDefault();
+	/* Act on the event */
+	var dbAddValues = [];
+
+	// check errors
+
+	// double entry error
+
+	// get values
+
+	// genarate link
+
+	// form insert
+
+	// update db
+
+	// update URL input
+	$('.linkToGo input').val(testUrl);
+});
+
+
+// copy link to Clipboard
+$('.linkToGo input').on('click', function() {
+	event.preventDefault();
+	/* Act on the event */
+	$(this).select();
+});
 
 
 
@@ -24,9 +82,20 @@ function testUrl () {
 
 
 
-console.log('testUrl =', testUrl);
 
-$(".genTestLink").text(testUrl);
+
+
+
+
+
+
+
+
+
+
+
+// console.log('testUrl =', testUrl);
+// $(".genTestLink").text(testUrl);
 
 
 
